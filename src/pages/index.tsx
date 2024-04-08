bef93d9868a43c6169dde27d176f16475bba644a
诸葛子房 <xiaoqiu2017wy@163.com>
2024-04-08 09:43:47 +0800

    feat:去掉动画

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -6,7 +6,6 @@ import { GetStaticProps, InferGetStaticPropsType } from 'next'
 import { PostFrontMatter } from 'types/PostFrontMatter'
 import NewsletterForm from '@/components/NewsletterForm'
 import Article from '@/components/Article'
-import Hero from '@/components/Hero'
 
 const MAX_DISPLAY = 5
 
@@ -20,7 +19,7 @@ export default function Home({ posts }: InferGetStaticPropsType<typeof getStatic
   return (
     <>
       <PageSEO title={siteMetadata.title} description={siteMetadata.description} />
-      <Hero welcome={siteMetadata.welcome} />
+      
       <div className="divide-y divide-gray-200 dark:divide-gray-700">
         <div className="space-y-2 pt-6 pb-8 md:space-y-5">
           <h1 className="text-3xl font-extrabold leading-9 tracking-tight text-gray-900 dark:text-gray-100 sm:text-4xl sm:leading-10 md:text-6xl md:leading-14">
